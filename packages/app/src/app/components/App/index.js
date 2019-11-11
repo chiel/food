d8b458548ca7230c6367e19696a38e0f93d51e56
Chiel Kunkels <chiel@kunkels.me>
2019-11-12 00:07:11 +0100

    feat(app): add coming soon page

--- packages/app/src/app/components/App/index.js
+++ packages/app/src/app/components/App/index.js
@@ -2,8 +2,15 @@ import './base.css';
 
 import React from 'react';
 
+import BrandedLayout from '../BrandedLayout';
+
 export default function App() {
 	return (
-		<div>Hello world!</div>
+		<BrandedLayout>
+			<h1>Hello there!</h1>
+			<p>
+				Soon there should be something more than a blank page here.
+			</p>
+		</BrandedLayout>
 	);
 }
