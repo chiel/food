e70635b2d8abacd675f69eecb8df2e6992e7a9f8
Chiel Kunkels <chiel@kunkels.me>
2019-11-19 00:30:36 +0100

    feat(api): add endpoint to get google auth url

--- packages/api/src/index.js
+++ packages/api/src/index.js
@@ -3,8 +3,26 @@ import express from 'express';
 const app = express();
 app.disable('x-powered-by');
 
-app.get('/', (req, res) => {
-	res.json({});
+app.get('/auth/google/url', (req, res) => {
+	const { redirectUrl } = req.query;
+	if (!redirectUrl) {
+		return res.status(400).json({
+			error: {
+				message: 'No `redirectUrl` provided',
+			},
+		});
+	}
+
+	const url = [
+		'https://accounts.google.com/o/oauth2/v2/auth',
+		`?scope=${encodeURIComponent('email profile')}`,
+		'&response_type=code',
+		'&access_type=online',
+		`&client_id=${process.env.AUTH_GOOGLE_CLIENT_ID}`,
+		`&redirect_uri=${encodeURIComponent(redirectUrl)}`,
+	].join('');
+
+	res.json({ url });
 });
 
 const port = 9153;
